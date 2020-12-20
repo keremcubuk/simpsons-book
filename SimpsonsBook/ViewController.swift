39068ab0733fa040ac1461ab7b8d3698d2550152
Kerem Çubuk <kerem.cubuk@eteration.com>
2020-12-20 14:23:08 +0300

    Initial Commit

---
+++ SimpsonsBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonsBook
+//
+//  Created by Kerem Çubuk on 20.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
